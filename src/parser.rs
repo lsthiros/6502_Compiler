de8714512a7b96e8d52888442fc9903f64e106a9
Louie Thiros <lsthiros@gmail.com>
2019-03-23 22:21:17 -0700

    Created recursive descent capture of "expression" production
    - Created recursive descent capture of "optional call" productions
    - Integrated "multi" functions for token stream
    - Refactored binary operation structure

--- src/parser.rs
+++ src/parser.rs
@@ -37,15 +37,25 @@ impl fmt::Display for BinOp {
 
 #[derive(Debug)]
 enum Factor {
-    Id(String),
+    Id{
+        id: String,
+        optional_call: Option<Vec<Box<AstExprNode>>>
+    },
     Numeric(f64)
 }
 
 impl fmt::Display for Factor {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Factor::Id(id) => {
-                write!(f, "{}", id)
+            Factor::Id{
+                id, optional_call
+            } => {
+                if optional_call.is_none() {
+                    write!(f, "Id: {}", id)
+                }
+                else {
+                    write!(f, "Call: {}", id)
+                }
             }
             Factor::Numeric(numeric) => {
                 write!(f, "{}", numeric)
@@ -66,6 +76,7 @@ enum AstExprNode {
         op_type: BinOp,
         next: Box<AstExprNode>
     },
+    SubNode(Box<AstExprNode>),
     Terminal(Factor)
 }
 
@@ -81,6 +92,9 @@ impl CreatesGraphviz for AstExprNode {
             } => {
                 format!("{}", op_type)
             }
+            AstExprNode::SubNode(_) => {
+                format!("()")
+            }
         }
     }
 
@@ -94,6 +108,9 @@ impl CreatesGraphviz for AstExprNode {
             } => {
                 let _ = op_type;
                 return vec![left.as_ref(), next.as_ref()];
+            }
+            AstExprNode::SubNode(sub_node) => {
+                return vec![sub_node.as_ref()];
             }
         }
     }
@@ -118,46 +135,74 @@ impl BinOp {
     }
 }
 
-trait ConstructsAst<I: Iterator<Item = LexerToken>> {
-    fn construct_ast(&self, token_stream: &mut TokenStream<I>) -> Result<Box<AstExprNode>, UnexpectedTokenError>;
-}
 
-struct BinOpConstructor<'a, I: Iterator<Item = LexerToken>> {
-    next: &'a ConstructsAst<I>,
-    op_type: TokenType
-}
-
-impl<'a, I: Iterator<Item = LexerToken>> ConstructsAst<I> for BinOpConstructor<'a, I> {
-    fn construct_ast(&self, token_stream: &mut TokenStream<I>) -> Result<Box<AstExprNode>, UnexpectedTokenError> {
-        let left: Box<AstExprNode> = self.next.construct_ast(token_stream)?;
-        if let Some(op) = token_stream.accept(self.op_type) {
-            let ret = AstExprNode::Node {
-                left: left,
-                op_type: BinOp::from_token_type(self.op_type, &op).unwrap(), 
-                next: self.construct_ast(token_stream)?
-            };
-            return Ok(Box::new(ret))
+fn factor<I>(token_stream: &mut TokenStream<I>) -> Result<Box<AstExprNode>, UnexpectedTokenError> where I: Iterator<Item = LexerToken> {
+    let result: Box<AstExprNode>;
+    let token = token_stream.expect_multi(&vec![TokenType::IDENTIFIER, TokenType::NUMBER, TokenType::L_PAREN])?;
+    match token.token_type {
+        TokenType::IDENTIFIER => {
+            let optional_call: Option<Vec<Box<AstExprNode>>> = get_optional_call(token_stream)?;
+            let factor = Factor::Id{
+                id: token.label.unwrap(),
+                optional_call: optional_call};
+            result = Box::new(AstExprNode::Terminal(factor))
         }
-
-        return Ok(left)
-    }
-}
-
-struct FactorTerminalConstructor {
-    // Zero size struct to hold trait for returning a terminating "Factor" node
-}
-
-impl<I: Iterator<Item = LexerToken>> ConstructsAst<I> for FactorTerminalConstructor {
-    fn construct_ast(&self, token_stream: &mut TokenStream<I>) -> Result<Box<AstExprNode>, UnexpectedTokenError> {
-        let factor: Factor;
-        if let Some(id) = token_stream.accept(TokenType::IDENTIFIER) {
-            factor = Factor::Id(id.label.unwrap());
-        }
-        else {
+        TokenType::NUMBER => {
             let numeric = token_stream.expect(TokenType::NUMBER)?;
-            factor = Factor::Numeric(numeric.number.unwrap());
+            let factor = Factor::Numeric(numeric.number.unwrap());
+            result = Box::new(AstExprNode::Terminal(factor))
         }
-        return Ok(Box::new(AstExprNode::Terminal(factor)))
+        TokenType::L_PAREN => {
+            let expression = sum_expr(token_stream)?;
+            let _ = token_stream.expect(TokenType::R_PAREN)?;
+            result = Box::new(AstExprNode::SubNode(expression));
+        }
+        _ => {
+            unreachable!()
+        }
+    }
+    return Ok(result)
+}
+
+type NextConstructor<I> = fn (&mut TokenStream<I>) -> Result<Box<AstExprNode>, UnexpectedTokenError>;
+
+fn construct_ast_inner<I>(token_stream: &mut TokenStream<I>, op_type: TokenType, next_constructor: NextConstructor<I>) -> Result<Box<AstExprNode>, UnexpectedTokenError> where I: Iterator<Item = LexerToken> {
+    let left: Box<AstExprNode> = next_constructor(token_stream)?;
+    if let Some(op) = token_stream.accept(op_type) {
+        let ret = AstExprNode::Node {
+            left: left,
+            op_type: BinOp::from_token_type(op_type, &op).unwrap(), 
+            next: next_constructor(token_stream)?
+        };
+        return Ok(Box::new(ret))
+    }
+
+    return Ok(left)
+}
+
+fn mult_expr<I>(token_stream: &mut TokenStream<I>) -> Result<Box<AstExprNode>, UnexpectedTokenError> where I: Iterator<Item = LexerToken> {
+    construct_ast_inner(token_stream, TokenType::MUL_OP, factor)
+}
+
+fn sum_expr<I>(token_stream: &mut TokenStream<I>) -> Result<Box<AstExprNode>, UnexpectedTokenError> where I: Iterator<Item = LexerToken> {
+    construct_ast_inner(token_stream, TokenType::SUM_OP, mult_expr)
+}
+
+fn get_optional_call<I>(token_stream: &mut TokenStream<I>) -> Result<Option<Vec<Box<AstExprNode>>>, UnexpectedTokenError> where I: Iterator<Item = LexerToken> {
+    if token_stream.accept(TokenType::L_PAREN).is_some() {
+        let mut arglist = Vec::<Box<AstExprNode>>::new(); 
+        if token_stream.accept(TokenType::R_PAREN).is_none() {
+            let mut continue_list = true;
+            while continue_list {
+                arglist.push(sum_expr(token_stream)?);
+                continue_list = token_stream.accept(TokenType::COMMA).is_some();
+            }
+        }
+        let _ = token_stream.expect(TokenType::R_PAREN)?;
+        return Ok(Some(arglist));
+    }
+    else {
+        Ok(None)
     }
 }
 
@@ -191,27 +236,9 @@ fn get_func_decl<I>(token_stream: &mut TokenStream<I>) -> Result<FuncDecl, Unexp
 pub fn parse_stream(token_stream: &Vec<LexerToken>) -> Result<String, UnexpectedTokenError> {
     let mut stream = TokenStream(token_stream.iter().cloned().peekable());
 
-    let term_parse = FactorTerminalConstructor {};
-    let mult_parse = BinOpConstructor::<std::iter::Cloned::<std::slice::Iter::<'_, LexerToken>>> {
-        op_type: TokenType::MUL_OP,
-        next: &term_parse
-    };
-    let sum_parse = BinOpConstructor::<std::iter::Cloned::<std::slice::Iter::<'_, LexerToken>>> {
-        op_type: TokenType::SUM_OP,
-        next: &mult_parse
-    };
-
-    let node: Box<AstExprNode> = sum_parse.construct_ast(&mut stream)?;
+    let node: Box<AstExprNode> = sum_expr(&mut stream)?;
     let result = Graphviz::from(node.as_ref() as &CreatesGraphviz);
-    // if let Some(def_token) = stream.accept(TokenType::DEF) {
-    //     let func = get_func_decl(&mut stream)?;
-    // }
-    // else if let Some(ext) = stream.accept(TokenType::EXTERN) {
-    //     let func = get_func_decl(&mut stream)?;
-    // }
-    // else {
-// 
-    // }
+
     result.write_file(String::from("./a.out"));
     return Ok(String::from("Ok"));
 }
